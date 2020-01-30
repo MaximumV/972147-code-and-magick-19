60a3f4f5fa6fc05d26b9dafe1cde0a8744b6a28f
Keks <keks@htmlacademy.ru>
2020-01-30 13:30:56 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/stat.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var CANVAS_WIDTH = 420;
+var CANVAS_HEIGHT = 270;
+var CANVAS_FILL = '#ffffff';
+var CANVAS_FILL_SHADOW = 'rgba(0, 0, 0, 0.7)';
+var CANVAS_LEFT_OFFSET = 155;
+var CANVAS_TOP_OFFSET = 35;
+var CANVAS_BOTTOM_OFFSET = CANVAS_HEIGHT - CANVAS_TOP_OFFSET / 10;
+var COLUMN_MAX_HEIGHT = 150;
+var COLUMN_WIDTH = 40;
+var COLUMN_OFFSET = 50;
+var FONT_STYLE = 'bold 16px PT Mono';
+var FONT_COLOR = '#000000';
+
+var searchMaxTime = function (times) {
+  var maxTime = times[0];
+  times.forEach(function (item) {
+    if (item > maxTime) {
+      maxTime = item;
+    }
+  });
+  return maxTime;
+};
+
+var getColumnHeightByTime = function (time, maxTime) {
+  return time * COLUMN_MAX_HEIGHT / maxTime;
+};
+
+var renderCloudWithShadow = function (ctx, x, y, offset) {
+  ctx.fillStyle = CANVAS_FILL_SHADOW;
+  ctx.fillRect(x + offset, y + offset, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillStyle = CANVAS_FILL;
+  ctx.fillRect(x, y, CANVAS_WIDTH, CANVAS_HEIGHT);
+};
+
+var getFillStyleByName = function (name) {
+  return name === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, 100%, 50%, ' + Math.random() + ')';
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  var maxTime = searchMaxTime(times);
+  var columnX = CANVAS_LEFT_OFFSET;
+  renderCloudWithShadow(ctx, 100, 10, 10);
+  ctx.fillStyle = FONT_COLOR;
+  ctx.font = FONT_STYLE;
+  ctx.fillText('Ура вы победили!', CANVAS_LEFT_OFFSET, CANVAS_TOP_OFFSET);
+  ctx.fillText('Список результатов:', CANVAS_LEFT_OFFSET, CANVAS_TOP_OFFSET + 25);
+  for (var i = 0; i < names.length; i++) {
+    var columnHeight = getColumnHeightByTime(times[i], maxTime);
+    ctx.fillStyle = FONT_COLOR;
+    ctx.fillText(names[i], columnX, CANVAS_BOTTOM_OFFSET);
+    ctx.fillText(Math.round(times[i]), columnX, CANVAS_BOTTOM_OFFSET - columnHeight - 35);
+    ctx.fillStyle = getFillStyleByName(name[i]);
+    ctx.fillRect(columnX, CANVAS_BOTTOM_OFFSET - columnHeight - 25, COLUMN_WIDTH, columnHeight);
+    columnX += COLUMN_WIDTH + COLUMN_OFFSET;
+  }
+};
