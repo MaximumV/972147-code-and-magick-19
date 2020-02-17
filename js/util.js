4e04a8c4e5b732caa5693acd5db991e92036edba
BeHustle <46607810+BeHustle@users.noreply.github.com>
2020-02-17 20:59:59 +0300

    Заносит объект util внутрь модуля

--- js/util.js
+++ js/util.js
@@ -1,22 +1,24 @@
 'use strict';
 
-window.util = (function () {
-  return {
-    getRandomArrayElement: function (array) {
-      return array[Math.floor(Math.random() * array.length)];
-    },
-    searchMaxArrayValue: function (times) {
-      var maxTime = times[0];
-      times.forEach(function (item) {
-        if (item > maxTime) {
-          maxTime = item;
-        }
-      });
-      return maxTime;
-    },
+(function () {
+  var getRandomArrayElement = function (array) {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+  var searchMaxArrayValue = function (times) {
+    var maxTime = times[0];
+    times.forEach(function (item) {
+      if (item > maxTime) {
+        maxTime = item;
+      }
+    });
+    return maxTime;
+  };
+
+  window.util = {
     ENTER_KEY: 'Enter',
     ESC_KEY: 'Escape',
-    HIDDEN_CLASS: 'hidden'
+    HIDDEN_CLASS: 'hidden',
+    getRandomArrayElement: getRandomArrayElement,
+    searchMaxArrayValue: searchMaxArrayValue
   };
 })();
-
