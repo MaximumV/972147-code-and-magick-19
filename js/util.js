1850e8bbc8fbd6848eee790ced920de9e027e055
BeHustle <46607810+BeHustle@users.noreply.github.com>
2020-02-21 01:56:35 +0300

    Получает данные о волшебниках по ajax. Добавляет отправку формы по ajax

--- js/util.js
+++ js/util.js
@@ -1,8 +1,26 @@
 'use strict';
 
 (function () {
+  var errorElement = document.createElement('div');
   var getRandomArrayElement = function (array) {
     return array[Math.floor(Math.random() * array.length)];
+  };
+  var shuffleArray = function (array) {
+    var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
+
+    while (currentIndex !== 0) {
+
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   };
   var searchMaxArrayValue = function (times) {
     var maxTime = times[0];
@@ -13,12 +31,29 @@
     });
     return maxTime;
   };
+  var showError = function (error) {
+    console.log(error);
+    errorElement.style.position = 'absolute';
+    errorElement.style.left = '0';
+    errorElement.style.right = '0';
+    errorElement.style = 'padding: 10px 0; z-index: 5; text-align: center; background-color: red; color: white';
+    errorElement.style.fontSize = '20px';
+    errorElement.textContent = error;
+    errorElement.style.display = 'block';
+    document.body.insertAdjacentElement('afterbegin', errorElement);
+  };
+  var hideError = function () {
+    errorElement.style.display = 'none';
+  };
 
   window.util = {
     ENTER_KEY: 'Enter',
     ESC_KEY: 'Escape',
     HIDDEN_CLASS: 'hidden',
     getRandomArrayElement: getRandomArrayElement,
-    searchMaxArrayValue: searchMaxArrayValue
+    shuffleArray: shuffleArray,
+    searchMaxArrayValue: searchMaxArrayValue,
+    showError: showError,
+    hideError: hideError
   };
 })();
