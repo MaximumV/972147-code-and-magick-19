1c5b7911f4cc57d52247c1975ca0d22e9c31fc30
Keks <keks@htmlacademy.ru>
2020-02-22 10:17:47 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/dialog.js
@@ -0,0 +1,119 @@
+'use strict';
+
+(function () {
+  var setupElement = document.querySelector('.setup');
+  var formElement = setupElement.querySelector('form');
+  var formSubmitElement = setupElement.querySelector('.setup-submit');
+  var closeSetupElement = document.querySelector('.setup-close');
+  var openSetupElement = document.querySelector('.setup-open');
+  var openSetupIconElement = openSetupElement.querySelector('.setup-open-icon');
+  var setupSimilarElement = document.querySelector('.setup-similar');
+  var nameInputElement = document.querySelector('.setup-user-name');
+  var dialogDragElement = document.querySelector('.upload');
+  var StartCoors = {
+    x: window.getComputedStyle(setupElement).getPropertyValue('top'),
+    y: window.getComputedStyle(setupElement).getPropertyValue('left')
+  };
+
+  var showSetup = function () {
+    setupElement.classList.remove(window.util.HIDDEN_CLASS);
+    setupElement.style.top = StartCoors.x;
+    setupElement.style.left = StartCoors.y;
+  };
+
+  var showSetupByEnterKey = function (evt) {
+    if (evt.key === window.util.ENTER_KEY) {
+      showSetup();
+    }
+  };
+
+  var showSimilarSetup = function () {
+    setupSimilarElement.classList.remove(window.util.HIDDEN_CLASS);
+  };
+
+  var hideSetup = function () {
+    setupElement.classList.add(window.util.HIDDEN_CLASS);
+  };
+
+  var hideSetupByEscKey = function (evt) {
+    if (!setupElement.classList.contains(window.util.HIDDEN_CLASS) && evt.key === window.util.ESC_KEY
+      && document.activeElement !== nameInputElement) {
+      hideSetup();
+    }
+  };
+
+  var hideSetupByEnterKey = function (evt) {
+    if (!setupElement.classList.contains(window.util.HIDDEN_CLASS) && evt.key === window.util.ENTER_KEY
+      && document.activeElement === closeSetupElement) {
+      hideSetup();
+    }
+  };
+
+  var onDragSetup = function (evt) {
+    evt.preventDefault();
+    var coors = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+      var shift = {
+        x: coors.x - moveEvt.clientX,
+        y: coors.y - moveEvt.clientY
+      };
+
+      coors = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setupElement.style.top = (setupElement.offsetTop - shift.y) + 'px';
+      setupElement.style.left = (setupElement.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogDragElement.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogDragElement.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  };
+
+
+  openSetupElement.addEventListener('click', showSetup);
+  openSetupIconElement.addEventListener('keydown', showSetupByEnterKey);
+  showSimilarSetup();
+  closeSetupElement.addEventListener('click', hideSetup);
+  dialogDragElement.addEventListener('mousedown', onDragSetup);
+  document.addEventListener('keydown', hideSetupByEscKey);
+  closeSetupElement.addEventListener('keydown', hideSetupByEnterKey);
+  formElement.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    var formSubmitContent = formSubmitElement.textContent;
+    formSubmitElement.setAttribute('disabled', 'disabled');
+    formSubmitElement.textContent = 'Загружаю...';
+    window.backend.save(new FormData(formElement), function () {
+      window.util.hideError();
+      hideSetup();
+      formSubmitElement.removeAttribute('disabled');
+      formSubmitElement.textContent = formSubmitContent;
+    }, function (error) {
+      window.util.showError(error);
+      formSubmitElement.removeAttribute('disabled');
+      formSubmitElement.textContent = formSubmitContent;
+    });
+  });
+})();
---
+++ 6/js/wizards.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function () {
+  var COUNT_WIZARDS = 4;
+
+  var wizardsListElement = document.querySelector('.setup-similar-list');
+  var wizardTemplate = document
+    .querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+  var renderWizard = function (wizardData) {
+    var wizardElement = wizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label')
+      .textContent = wizardData.name;
+    wizardElement.querySelector('.wizard-coat')
+      .style.fill = wizardData.colorCoat;
+    wizardElement.querySelector('.wizard-eyes')
+      .style.fill = wizardData.colorEyes;
+    return wizardElement;
+  };
+
+  var generateWizards = function (wizardsData) {
+    var wizards = window.util.shuffleArray(wizardsData);
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < COUNT_WIZARDS; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    wizardsListElement.appendChild(fragment);
+  };
+
+  window.backend.load(generateWizards, window.util.showError);
+})();
+
